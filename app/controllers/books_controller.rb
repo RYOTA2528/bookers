3cc3f38c412b4c29950b63c206a92dbeba7825d6
RYOTA2528 <1216022@gmail.com>
2021-07-14 08:59:04 +0000

    [Update]フラッシュメッセージ及びバリデーションの設定完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,19 @@
 class BooksController < ApplicationController
    def index
     @books = Book.all
+    @book = Book.new
    end
 
-  def new
-    @book = Book.new
-  end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book)
     flash[:notice]='Book was successfully created.'
+    else
+    @books = Book.all
+    render :index
+
     end
   end
   def show
@@ -24,20 +26,23 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-   @book.update(book_params)
+   if @book.update(book_params)
    redirect_to book_path(@book)
    flash[:notice]='Book was successfully created.'
+   else
+    render :edit
+   end
   end
 
   def destroy
     book =Book.find(params[:id])
     book.destroy
     redirect_to books_path
-    flash[:notice]='Book was successfully created.'
+    flash[:notice]='Book was successfully destroyed.'
   end
 
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
